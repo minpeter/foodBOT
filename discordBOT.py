c9a34958b01a6c51f2470ea4aad732770104d429
minpeter <minpeter>
2021-03-18 22:50:54 +0900

    ver 2

---
+++ discordBOT.py
@@ -0,0 +1,50 @@
+import time
+import datetime
+import discord
+from food import food
+
+
+client = discord.Client()
+discordbottoken="ODE4MTQ0OTkzODcyOTY5Nzcx.YETy5g.2eIWgNHOBqmgdNyyF6K8jufMnKg"
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print(client.user.name)
+    print("ready")
+    await client.change_presence(status=discord.Status.online, activity = discord.Game("!오늘급식 or !ㅇㄴㄱㅅ"))
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!오늘급식") or message.content.startswith("!ㅇㄴㄱㅅ"):
+        today = food(datetime.datetime.now().month, datetime.datetime.now().day)
+
+        time = today[1].strip()
+        diet = today[3].strip()
+        calorie = today[4].strip()
+
+        img = "이미지가 존재하지 않음"
+
+        if len(today) == 6:
+            img = "http://hansei.sen.hs.kr"+today[5].strip()
+        
+        await message.channel.send(f"```등록일: {time}\n식단: {diet}\n칼로리: {calorie}```\n{img}")
+
+
+    if message.content.startswith("!내일급식") or message.content.startswith("!ㄴㅇㄱㅅ"):
+            today = food(datetime.datetime.now().month, datetime.datetime.now().day + 1)
+
+            time = today[1].strip()
+            diet = today[3].strip()
+            calorie = today[4].strip()
+
+            img = "이미지가 존재하지 않음"
+
+            if len(today) == 6:
+                img = "http://hansei.sen.hs.kr"+today[5].strip()
+            
+            await message.channel.send(f"```등록일: {time}\n식단: {diet}\n칼로리: {calorie}```\n{img}")
+
+client.run(discordbottoken)
---
+++ food.py
@@ -0,0 +1,33 @@
+
+import time
+import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver.get('http://hansei.sen.hs.kr/50649/subMenu.do')
+
+def food (monthF, dayF):
+    driver.find_element_by_link_text(f'{monthF}월 {dayF}일').click()
+
+    time.sleep(1)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.select_one('table.tbType02 > tbody')
+    driver.find_element_by_xpath('//*[@id="divLayerMlsvPopup"]/div/div/div/button').click()
+
+    resultlist = []
+
+    for i in table.find_all("td"):
+        result = i.string
+        resultlist.append(result)
+
+    if len(resultlist) == 6:
+        del resultlist[5]
+        resultlist.append(table.find("img")["src"])
+
+    return resultlist
