d76a7218add19b9cf0b41e07d0336c36e433471e
minpeter <kali2005611@gmail.com>
2021-04-01 20:55:37 +0900

    .

--- food.py
+++ food.py
@@ -13,10 +13,10 @@ def food(m,d):
     dt = datetime.datetime.now()
     response = requests.get(URL+f"&MLSV_YMD={dt.strftime('%Y%m%d')}").json()
     print(URL+f"&MLSV_YMD={dt.strftime('%Y%m%d')}")
-    date_head = response['mealServiceDietInfo'][0]['head']
+    #date_head = response['mealServiceDietInfo'][0]['head']
     #[0]은 list_total_count
     #[1]은 {'RESULT': {'CODE': 'INFO-000', 'MESSAGE': '정상 처리되었습니다.'}}
-    date_body = response['mealServiceDietInfo'][1]['row'][0]
+    #date_body = response['mealServiceDietInfo'][1]['row'][0]
     return date_body
     
 
